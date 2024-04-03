9a0e4ff4328b58b48163022db4833374b029ffc1
Larisa Moșuț <larisa.mosut@emanuel.ro>
2024-04-03 18:31:11 +0300

    am facut o cutie

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const box1 = { x: 10, y: 10, latura: 50, culoare: 'red', numar: 1 };
+
 function setup() {
     createCanvas(600, 400);
+    textSize(15); 
+    textAlign(CENTER, CENTER);
 }
 
 function draw() {
-    background("gray")
+    background("gray");
+    fill(box1.culoare);
+    rect(box1.x, box1.y, box1.latura, box1.latura);
+    fill("black"); 
+    text(box1.numar, box1.x + box1.latura / 2, box1.y + box1.latura / 2);
+
 }
